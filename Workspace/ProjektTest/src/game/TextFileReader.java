c6205a4991d44b8ed0cf0144b2024e4365107635
Robin Söderholm <robin.soderholm87@gmail.com>
2016-04-13 08:07:40 +0200

    Testar lite grejer i filereader
    Klassen läser från textfil och printar ut i konsolfönstret. Fortsätter
    jobba men committar sålänge

--- Workspace/ProjektTest/src/game/TextFileReader.java
+++ Workspace/ProjektTest/src/game/TextFileReader.java
@@ -1,5 +1,23 @@
 package game;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TextFileReader {
 
+	public static void main(String[] args) {
+		try {
+			FileReader fr = new FileReader("files/testscript.txt");
+			BufferedReader br = new BufferedReader(fr);
+			
+			String str;
+			while((str = br.readLine()) != null) {
+				System.out.println(str + "\n");
+			}
+			br.close();
+		} catch (IOException e) {
+			System.out.print("File not found");
+		}
+ 	}
 }
