051d95862bbb47d8d359b23b5747ffcefb1fdbd9
rsoderholm <robin.soderholm87@gmail.com>
2016-05-02 10:44:13 +0200

    FileReader

--- Workspace/ProjektTest/src/game/TextFileReader.java
+++ Workspace/ProjektTest/src/game/TextFileReader.java
@@ -1,33 +1,52 @@
 package game;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.LineNumberReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author robinsoderholm
+ *
+ */
 
 public class TextFileReader {
+	String line = null;
+	private HashMap<String, String> map = new HashMap<>();
 
-	private ArrayList<String> list = new ArrayList<>();
+	public void read(String filepath) throws IOException {
 
-	public void textReader(String filepath) {
 		try {
-			LineNumberReader lr = new LineNumberReader(new FileReader(filepath));
-			String line = " ";
 
-			while ((line = lr.readLine()) != null) {
-				list.add(line);
-				System.out.println(line);
-
+			Scanner scanner = new Scanner(new File(filepath));
+			String line = "";
+			while (scanner.hasNextLine()) {
+				line += scanner.nextLine();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
+			String[] splitParagraph = line.split("#");
+			for (int i = 0; i < splitParagraph.length; i++) {
+				System.out.println(splitParagraph[i]);
+			}
+
 		}
+
+		catch (Exception ex) {
+			System.out.println("Problem");
+
+		}
+
 	}
 
-	public static void main(String[] args) {
-		TextFileReader t = new TextFileReader();
-		t.textReader("files/testscript.txt");
-
+	public static void main(String[] args) throws IOException {
+		TextFileReader reader = new TextFileReader();
+		// reader.readText("files/testscript.txt");
+		reader.read("files/testscript.txt");
 	}
 
 }
